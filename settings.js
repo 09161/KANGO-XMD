9d11e61cc09d760f3b55fd9f60569fa9c9ec6d42
09161 <136868979+09161@users.noreply.github.com>
2025-05-14 02:16:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || '' KANGO~7Qdwib7S#FRHvqNa-4vD92A3Y2o35lLx6FKb8zFXcDEoKOXhpCKk
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -29,11 +29,11 @@ global.ownernumber = process.env.OWNER_NUMBER || '2349161974630'
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'Promise-MD' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿" 
+global.packname = process.env.STICKER_PACK_NAME || "Promise-𝙈𝘿" 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
